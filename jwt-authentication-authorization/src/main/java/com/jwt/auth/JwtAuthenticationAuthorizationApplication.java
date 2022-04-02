61f4a033876892f068acf75acec36e1e33358822
sadakhatalikhan <45699025+sadakhatalikhan@users.noreply.github.com>
2022-04-02 04:08:20 +0400

    initial commit

---
+++ jwt-authentication-authorization/src/main/java/com/jwt/auth/JwtAuthenticationAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationAuthorizationApplication.class, args);
+	}
+
+}
